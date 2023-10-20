78d487d54b96901594ba7df3ef1d1a9cb73ed25e
Ubuntu <ubuntu@ip-172-31-95-84.ec2.internal>
2023-10-20 12:42:27 +0000

    Final

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -31,3 +31,14 @@ mv /tmp/iaw-practica-lamp/src/* /var/www/html
 sed -i "s/database_name_here/$DB_NAME/" /var/www/html/config.php
 sed -i "s/username_here/$DB_USER/" /var/www/html/config.php
 sed -i "s/password_here/$DB_PASSWORD/" /var/www/html/config.php
+
+#Importamos el script de la base de datos
+
+mysql -u root < /tmp/iaw-practica-lamp/db/database.sql
+
+
+#Creamos el usuario de la base de datos y le asignamos privilegios
+
+mysql -u root <<< "DROP USER IF EXISTS '$DB_USER'@'%'"
+mysql -u root <<< "CREATE USER '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWORD'"
+mysql -u root <<< "GRANT ALL PRIVILEGES ON $DB_NAME.* TO $DB_USER@'%'"
